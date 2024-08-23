143d44c799fc9d32453cf5d55a977bd860070d3c
Kevin <esfk070@gmail.com>
2024-08-23 11:09:23 -0600

    Configure Hibernate, test Userentity

--- JPASpringFit/src/test/java/com/skilldistillery/springfit/entities/UserTest.java
+++ JPASpringFit/src/test/java/com/skilldistillery/springfit/entities/UserTest.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.springfit.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -8,27 +9,43 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPASpringFit");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em=emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
+
 	}
 
 }
