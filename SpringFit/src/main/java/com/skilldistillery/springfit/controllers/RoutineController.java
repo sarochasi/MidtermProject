856f2f1f32e35c93e8c57341de0a5a02321a8d6e
Kevin <esfk070@gmail.com>
2024-08-29 16:44:01 -0600

    Rob brings the project back alive

--- SpringFit/src/main/java/com/skilldistillery/springfit/controllers/RoutineController.java
+++ SpringFit/src/main/java/com/skilldistillery/springfit/controllers/RoutineController.java
@@ -14,6 +14,7 @@ import com.skilldistillery.springfit.data.RoutineDAO;
 import com.skilldistillery.springfit.data.RoutineWorkoutDAO;
 import com.skilldistillery.springfit.data.WorkoutDAO;
 import com.skilldistillery.springfit.entities.Routine;
+import com.skilldistillery.springfit.entities.RoutineWorkout;
 import com.skilldistillery.springfit.entities.User;
 import com.skilldistillery.springfit.entities.Workout;
 
@@ -37,26 +38,20 @@ public class RoutineController {
 		User loggedInUser = (User) session.getAttribute("loggedInUser");
 		if (loggedInUser != null) {
 			Routine newRoutine = routineDao.createNewRoutine(routine, loggedInUser.getId());
-			
 			List<Workout> workouts = workoutDao.getWorkoutByUserId(loggedInUser.getId());
 			mv.addObject("workouts", workouts);
 			mv.addObject("routineId", newRoutine.getId());
-			mv.setViewName("createRoutine");
+			mv.addObject("routine", newRoutine);
 			System.out.println();
 			System.out.println();
 			System.out.println(routine);
 			System.out.println();
-			
+
 			mv.setViewName("createRoutine");
-			
-		}
-		else {
+		} else {
 			mv.setViewName("home");
 		}
-		
-		
 		return mv;
-		
 	}
 	@RequestMapping(path = "createRoutine.do", params = "routineId")
 	public ModelAndView createRoutine(@RequestParam("routineId") int routineId, HttpSession session) {
@@ -102,6 +97,52 @@ System.out.println();
 		
 		return mv;
 		
+	}
+	
+	//---------------Receives routineId and WorkoutId, Command Object of RoutineWorkout -----------------
+//	@RequestMapping(path = "recieveRoutineDayAndWorkoutAndCreateRoutineWorkout.do", params = "routineId")
+	@RequestMapping(path = "addWorkoutToRoutine.do", params = "routineId", method = RequestMethod.POST)
+	public String getRoutineDay(@RequestParam("routineId") int routineId, HttpSession session,
+			RoutineWorkout routineWorkout) {
+		ModelAndView mv = new ModelAndView();
+		User loggedInUser = (User) session.getAttribute("loggedInUser");
+		if (loggedInUser != null) {
+
+			Routine routine = routineDao.getRoutineById(routineId);
+//			Workout workout = workoutDao.getWorkoutById(workoutId);
+			RoutineWorkout newRoutineWorkout = routineWorkoutDao.createRoutineWorkout(0, 0, routineId, routineWorkout);
+
+			mv.setViewName("createRoutine");
+			mv.addObject("routineId", routineId);
+
+//			Routine newRoutine = routineDao.getRoutineById(routineId);
+//			newRoutine.addRoutineWorkout(newRoutineWorkout);
+
+			List<Workout> workouts = workoutDao.getWorkoutByUserId(loggedInUser.getId());
+//			return "redirect:createRoutine.do?routineId=" +newRoutine.getId() + "&workouts="+workouts;
+//			return "redirect:goTocreateRoutinePage.do";		//works but creates multiple routines
+			return "redirect:addWorkoutToRoutine.do?routineId=" + routineId;
+		} else {
+			mv.setViewName("home");
+			return "home";
+		}
+	}
+
+	@RequestMapping(path = "addWorkoutToRoutine.do", params = "routineId", method = RequestMethod.GET)
+	public ModelAndView goToAddWorkoutToRoutine(@RequestParam("routineId") int routineId, HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+		User loggedInUser = (User) session.getAttribute("loggedInUser");
+		if (loggedInUser != null) {
+			List<Workout> workouts = workoutDao.getWorkoutByUserId(loggedInUser.getId());
+			Routine routine = routineDao.getRoutineById(routineId);
+			mv.addObject("workouts", workouts);
+			mv.addObject("routineId", routine.getId());
+			mv.addObject("routine", routine);
+			mv.setViewName("createRoutine");
+		} else {
+			mv.setViewName("home");
+		}
+		return mv;
 	}
 	
 	@RequestMapping(path = "deleteRoutine.do", params = "routineId", method = RequestMethod.POST)
